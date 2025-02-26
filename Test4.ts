6fc6f8097eae131baa03a24710e4f886a86272bd
CamelqTechnology <sangeetavanam87@gmail.com>
2025-02-26 14:04:28 +0530

    angular files are created

---
+++ Test4.ts
@@ -0,0 +1,8 @@
+// my.component.ts
+@Component({
+    templateUrl: './my.component.html'
+  })
+  
+  export class MyComponent {
+    value:type = /* some value of type */;
+  }
---
+++ Test5.ts
@@ -0,0 +1,10 @@
+// my.component.ts
+@Component({
+    templateUrl: './my.component.html'
+  })
+  
+  export class MyComponent {
+    handler(event):void {
+        // function does stuff
+    }
+  }
